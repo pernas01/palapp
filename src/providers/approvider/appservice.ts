55d734a773b97613bade7b7c3bafd1383e9141f1
Per <per.naslund@gmail.com>
2019-02-10 15:16:37 +0100

    added adverts for stores

--- src/providers/approvider/appservice.ts
+++ src/providers/approvider/appservice.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class AppService {
 
-  private stores: Store[] = [{ id: "1", storeName: "ICA Nära Blå Center", latitude: 63.170065, longitude: 14.663709, adverts: [], showAdverts: false },
-  { id: "2", storeName: "ICA Supermarket Traktören", latitude: 63.179275, longitude: 14.638412, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png" }, { imageUrl: "assets/imgs/adverts/10percent.png" }, { imageUrl: "assets/imgs/adverts/10percent.png" }], showAdverts: false },
-  { id: "3", storeName: "ICA Kvantum Lillänge", latitude: 63.170730, longitude: 14.691163, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png" }], showAdverts: false },
-  { id: "4", storeName: "ICA Supermarket Matmästaren", latitude: 63.179791, longitude: 14.650953, adverts: [], showAdverts: false },
-  { id: "5", storeName: "ICA Maxi Stormarknad", latitude: 63.192805, longitude: 14.651058, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png"}, {imageUrl: "img44541"}, {imageUrl: "assets/imgs/adverts/10percent.png"}, {imageUrl: "assets/imgs/adverts/10percent.png" }], showAdverts: false },
-  { id: "6", storeName: "ICA Supermarket Odenhallen", latitude: 63.155881, longitude: 14.683254, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png"}, {imageUrl: "assets/imgs/adverts/10percent.png"}, {imageUrl: "assets/imgs/adverts/10percent.png" }], showAdverts: false }];
+  private stores: Store[] = [{ id: "1", storeName: "ICA Nära Blå Center", latitude: 63.170065, longitude: 14.663709, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png" }, {imageUrl: "assets/imgs/adverts/chips.png"}, {imageUrl: "assets/imgs/adverts/coffee.png"}], showAdverts: false },
+  { id: "2", storeName: "ICA Supermarket Traktören", latitude: 63.179275, longitude: 14.638412, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png" }, { imageUrl: "assets/imgs/adverts/chocolate.png" }, { imageUrl: "assets/imgs/adverts/digestive.png" }], showAdverts: false },
+  { id: "3", storeName: "ICA Kvantum Lillänge", latitude: 63.170730, longitude: 14.691163, adverts: [{ imageUrl: "assets/imgs/adverts/5percent.png" }, {imageUrl: "assets/imgs/adverts/coffee.png"}, {imageUrl: "assets/imgs/adverts/chips.png"}], showAdverts: false },
+  { id: "4", storeName: "ICA Supermarket Matmästaren", latitude: 63.179791, longitude: 14.650953, adverts: [{ imageUrl: "assets/imgs/adverts/10percent.png" }, {imageUrl: "assets/imgs/adverts/digestive.png"}, {imageUrl: "assets/imgs/adverts/chocolate.png"}], showAdverts: false },
+  { id: "5", storeName: "ICA Maxi Stormarknad", latitude: 63.192805, longitude: 14.651058, adverts: [{ imageUrl: "assets/imgs/adverts/5percent.png"}, {imageUrl: "assets/imgs/adverts/coffee.png"}, {imageUrl: "assets/imgs/adverts/chocolate.png"}], showAdverts: false },
+  { id: "6", storeName: "ICA Supermarket Odenhallen", latitude: 63.155881, longitude: 14.683254, adverts: [{ imageUrl: "assets/imgs/adverts/5percent.png"}, {imageUrl: "assets/imgs/adverts/digestive.png"}, {imageUrl: "assets/imgs/adverts/chips.png" }], showAdverts: false }];
   private visableWindows: google.maps.InfoWindow[] = [];
   private choosenStore: Store;
   private chooseStore: Subject<Store> = new Subject();
